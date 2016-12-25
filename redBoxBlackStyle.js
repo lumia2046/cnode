78d6031aa270c5c5b0a206963e9af345203bb84a
lumia2046 <1628560832@qq.com>
2016-12-25 09:58:13 +0800

    '错误提示插件的样式'

---
+++ redBoxBlackStyle.js
@@ -0,0 +1,21 @@
+module.exports = {
+    style: {
+        redbox: {
+            boxSizing: 'border-box',
+            fontFamily: 'sans-serif',
+            position: 'fixed',
+            padding: 10,
+            top: '0px',
+            left: '0px',
+            bottom: '0px',
+            right: '0px',
+            width: '100%',
+            background: 'rgba(0, 0, 0, 0.8)',
+            color: 'red',
+            zIndex: 9999,
+            textAlign: 'left',
+            fontSize: '16px',
+            lineHeight: 1.2
+        }
+    }
+};
